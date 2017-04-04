5930d05d753279acd040fef1d77944aad2d1f9ab
Yohanan radchenko <yohananradchenko@gmail.com>
2017-04-04 11:18:05 -0400

    create session controller and complete user model auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,37 @@
 #
 
 class User < ApplicationRecord
+  validates :username, :email, uniqueness: { case_sensitive: false }, presence: true
+  validates :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  after_initialize :ensure_session_token!
+
+  attr_reader :password
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    password_hash = BCrypt::Password.new(self.password_digest)
+    password_hash.is_password?(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil if user.nil?
+    return nil unless user.is_password?(password)
+    user
+  end
+
+  def ensure_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+  end
 end
