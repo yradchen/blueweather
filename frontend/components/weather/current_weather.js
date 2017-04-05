ac575c45c53adb546c3317077c4ce9f0d31686fc
Yohanan radchenko <yohananradchenko@gmail.com>
2017-04-05 14:25:00 -0400

    setup current weather to extract weather for current weather and daily weather

--- frontend/components/weather/current_weather.js
+++ frontend/components/weather/current_weather.js
@@ -7,15 +7,48 @@ class CurrentWeather extends React.Component {
   }
 
   weatherSummary(date) {
-    return this.props.weather[date].summary;
+    const weather = this.props.weather[date];
+    
+    return (
+      <div>
+        <p>Current Weather</p>
+        <p>{weather.summary}</p>
+        <p>FUTURE CURRENT WEATHER ICON: {weather.icon}</p>
+        <p>Current Temp: {weather.temperature}</p>
+        <p>Chance of Precipitation: {weather.precipProbability * 100}%</p>
+      </div>
+    );
+    // currently find temperature
+  }
+
+  weatherWeek() {
+    const dailyWeather = this.props.weather.daily.data.slice(1);
+    return dailyWeather.map(datum  => {
+      const date = new Date(datum.time * 1000).toDateString();
+
+      return (
+        <div key={date}>
+          <p>{date}</p>
+          <p>{datum.summary}</p>
+          <p>FUTURE IMAGE FOR: {datum.icon}</p>
+          <p>Min Temp: {datum.temperatureMin}</p>
+          <p>Max Temp: {datum.temperatureMax}</p>
+          <p>Chance of {datum.precipType}: {datum.precipProbability * 100}%</p>
+        </div>
+      );
+    });
   }
 
   render() {
     if (this.props.weather.currently === undefined) return null;
     return (
       <div>
-        <p>Current Summary: {this.weatherSummary("currently")}</p>
-        <p>Week Summary: {this.weatherSummary("daily")}</p>
+        <section>{this.weatherSummary("currently")}</section>
+
+        <section>
+          {/* <p>Week Summary: {this.weatherSummary("daily")}</p> */}
+          <section>Week Days {this.weatherWeek()}  </section>
+        </section>
       </div>
     );
   }
@@ -23,3 +56,8 @@ class CurrentWeather extends React.Component {
 }
 
 export default CurrentWeather;
+
+// this.props.weather.currently.temperature
+// this.props.weather.currently.icon
+// this.props.weather.currently.precipProbability - chance of rain
+// this.props.weather.currently.time
